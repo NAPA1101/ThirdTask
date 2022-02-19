f5cc29720aa911a93ff7075340629658ad73b800
Yegor <77298094+NAPA1101@users.noreply.github.com>
2022-02-19 17:11:30 +0200

    Add files via upload

--- ThirdTask/Block/Index.php
+++ ThirdTask/Block/Index.php
@@ -4,32 +4,110 @@ namespace Perspective\ThirdTask\Block;
 
 use Magento\Framework\View\Element\Template\Context;
 use Magento\Framework\Registry;
+use Magento\CatalogRule\Model\Rule;
+use Perspective\ThirdTask\Helper\Data;
 
 class Index extends \Magento\Framework\View\Element\Template
 {
+    /**
+     * @var Registry
+     */
     protected $_registry;
 
+    /**
+     * @var Rule
+     */
+    protected $_rule;
+
+    /**
+     * @var Data
+     */
+    protected $_helper;
+
+    /**
+     * @param Context $context
+     * @param Registry $registry
+     * @param Rule $rule
+     * @param Data $helper
+     * @param array $data
+     */
     public function __construct(
         Context  $context,
         Registry $registry,
+        Rule $rule,
+        Data $helper,
         array    $data = [])
     {
         parent::__construct($context, $data);
         $this->_registry = $registry;
+        $this->_rule = $rule;
+        $this->_helper = $helper;
 
     }
 
+    /**
+     * @return mixed|null
+     */
     public function getCurrentProduct()
     {
         return $this->_registry->registry('current_product');
     }
 
+    /**
+     * @return string|void
+     */
     public function getSpecialDate()
     {
-        $endSpecialDate = $this->getCurrentProduct()->getSpecialToDate();
+        if($this->_helper->getGeneralConfig('enable')) {
+            $endSpecialDate = $this->getCurrentProduct()->getSpecialToDate();
             if ($endSpecialDate) {
-                $date = date_create($endSpecialDate);
-                return $date->Format('Y-m-d');
+                $data = date_create($endSpecialDate);
+                return date_format($data, 'Y/m/d');
             }
+        }
+    }
+
+    /**
+     * @param $ruleId
+     * @return string|void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function getRuleDate($ruleId)
+    {
+        if($this->_helper->getGeneralConfig('enable')) {
+            $rules = $this->_rule->getResourceCollection()->addFieldToFilter('rule_id', $ruleId);
+            $productId = $this->getCurrentProduct()->getId();
+            if ($rules) {
+                foreach ($rules as $rule) {
+                    if (isset($rule->getMatchingProductIds()[$productId][1])) {
+                        $data = date_create($rule->getToDate());
+                        return date_format($data, 'Y/m/d');
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSpecialEnable() {
+        return $this->_helper->getGeneralConfig('specialEnable');
+    }
+
+    /**
+     * @return array
+     */
+    public function getRuleEnable() {
+        $mass[] = $this->_helper->getGeneralConfig('rulePensionersEnable');
+        $mass[] = $this->_helper->getGeneralConfig('ruleTestEnable');
+        return $mass;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComparison() {
+        return $this->_helper->getGeneralConfig('min');
     }
 }
---
+++ ThirdTask/Helper/Data.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Perspective\ThirdTask\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+
+    const XML_PATH_COUNTDOWN = 'countdown/';
+
+    /**
+     * @param $field
+     * @param $storeId
+     * @return mixed
+     */
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field, ScopeInterface::SCOPE_STORE, $storeId
+        );
+    }
+
+    /**
+     * @param $code
+     * @param $storeId
+     * @return mixed
+     */
+    public function getGeneralConfig($code, $storeId = null)
+    {
+
+        return $this->getConfigValue(self::XML_PATH_COUNTDOWN .'general/'. $code, $storeId);
+    }
+
+}
